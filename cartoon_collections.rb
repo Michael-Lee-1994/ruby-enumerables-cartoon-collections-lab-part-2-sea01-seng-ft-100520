6966b260078b305ee5157284a24d26602e94f927
Michael Lee <michael.l33.1994@gmail.com>
2020-09-24 23:06:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.any? do |element|
     if element == valid_calls.any?
+      element
     end
-    element
   end
 end
